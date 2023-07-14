03b390762f3e7b47795a4985d221a5ad010fba18
tosiron <tosironj@gmail.com>
2023-07-14 20:59:02 +0100

    edited the mail.js

--- utils/mail.js
+++ utils/mail.js
@@ -78,8 +78,8 @@ a{
 </head>
 <body>
 <!-- partial:index.partial.html -->
-<div style="font-family: Helvetica,Arial,sans-serif;min-width:1000px;overflow:auto;line-height:2">
-<div style="margin:10px auto;width:70%;padding:10px 0">
+<div style="font-family: Helvetica,Arial,sans-serif;min-width:100%px;overflow:auto;line-height:2">
+<div style="margin:3px auto;width:70%;padding:5px 0">
   <div style="border-bottom:1px solid #eee">
     <a href="" style="font-size:1.4em;color: #00466a;text-decoration:none;font-weight:600">I.S.E.E.A</a>
   </div>
@@ -172,8 +172,8 @@ a{
 </head>
 <body>
 <!-- partial:index.partial.html -->
-<div style="font-family: Helvetica,Arial,sans-serif;min-width:1000px;overflow:auto;line-height:2">
-<div style="margin:10px auto;width:70%;padding:10px 0">
+<div style="font-family: Helvetica,Arial,sans-serif;min-width:100%;overflow:auto;line-height:2">
+<div style="margin:3px auto;width:70%;padding:5px 0">
   <div style="border-bottom:1px solid #eee">
     <a href="" style="font-size:1.4em;color: #00466a;text-decoration:none;font-weight:600">I.S.E.E.A</a>
   </div>
