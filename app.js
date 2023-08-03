df80ec29d3ad96095c916a69e3c6e66a619fec14
tosiron <tosironj@gmail.com>
2023-08-03 11:24:51 +0100

    refactored app.js

--- app.js
+++ app.js
@@ -9,46 +9,8 @@ const helmet = require("helmet");
 const xss = require("xss-clean");
 const cors = require("cors");
 const mongoSanitize = require("express-mongo-sanitize");
-app.use(cookieParser(process.env.JWT_SECRET));
 
-// import routes
-const AuthRouter = require("./routes/auth");
-const ImpactRouter = require("./routes/impact");
-const InterventionRouter = require("./routes/intervention");
-const ProjectRouter = require("./routes/projects");
-const MemberRouter = require("./routes/members");
-const NumberRouter = require("./routes/numbers");
-const ReportRouter = require("./routes/annual-report");
-
-// import middlewares
-const notFoundHandler = require("./middleware/not-found");
-const errorHandler = require("./middleware/errors-handler");
-const connectDB = require("./connectdb/connectdb");
-const { StatusCodes } = require("http-status-codes");
-
-app.use(express.json());
-app.use('/uploads', express.static('uploads'));
-app.use(
-  session({
-    secret: process.env.JWT_SECRET,
-    resave: false,
-    saveUninitialized: false,
-  })
-);
-
-// routes
-app.use("/api/v1/auth", AuthRouter);
-app.use("/api/v1/impacts", ImpactRouter);
-app.use("/api/v1/interventions", InterventionRouter);
-app.use("/api/v1/projects", ProjectRouter);
-app.use("/api/v1/members", MemberRouter);
-app.use("/api/v1/numbers", NumberRouter);
-app.use("/api/v1/reports", ReportRouter);
-app.get("/",(req,res)=>{
-  res.status(StatusCodes.OK).send("Welcome to ISEEA backend site")
-})
-
-// middlewares
+// Middlewares
 app.set("trust proxy", 1);
 app.use(
   rateLimiter({
@@ -57,19 +19,62 @@ app.use(
   })
 );
 app.use(helmet());
-app.use(cors());
+app.use(cors()); 
+app.options('*', cors()); 
 app.use(xss());
 app.use(mongoSanitize());
 app.use(notFoundHandler);
 app.use(errorHandler);
-// experimental
-app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-  next();
+
+// Express JSON Parser and Cookie Parser
+app.use(express.json());
+app.use(cookieParser(process.env.JWT_SECRET));
+
+// Session Middleware
+app.use(
+  session({
+    secret: process.env.JWT_SECRET,
+    resave: false,
+    saveUninitialized: false,
+  })
+);
+
+// Experimental CORS Setup
+// app.use((req, res, next) => {
+//   res.setHeader("Access-Control-Allow-Origin", "*");
+//   res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+//   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+//   next();
+// });
+
+// Import routes
+const AuthRouter = require("./routes/auth");
+const ImpactRouter = require("./routes/impact");
+const InterventionRouter = require("./routes/intervention");
+const ProjectRouter = require("./routes/projects");
+const MemberRouter = require("./routes/members");
+const NumberRouter = require("./routes/numbers");
+const ReportRouter = require("./routes/annual-report");
+
+// Routes
+app.use("/api/v1/auth", AuthRouter);
+app.use("/api/v1/impacts", ImpactRouter);
+app.use("/api/v1/interventions", InterventionRouter);
+app.use("/api/v1/projects", ProjectRouter);
+app.use("/api/v1/members", MemberRouter);
+app.use("/api/v1/numbers", NumberRouter);
+app.use("/api/v1/reports", ReportRouter);
+
+// Root route
+app.get("/", (req, res) => {
+  res.status(StatusCodes.OK).send("Welcome to ISEEA backend site");
 });
 
+// Error handler middleware (404 and other errors)
+app.use(notFoundHandler);
+app.use(errorHandler);
+
+const { StatusCodes } = require("http-status-codes");
 
 const port = process.env.PORT || 5000;
 
