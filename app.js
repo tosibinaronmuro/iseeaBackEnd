764fef7bad3ba40475837083f92c40fdf04c22a3
tosiron <tosironj@gmail.com>
2023-08-03 11:15:14 +0100

    tempoary changes

--- app.js
+++ app.js
@@ -64,7 +64,7 @@ app.use(notFoundHandler);
 app.use(errorHandler);
 // experimental
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+  res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
   next();
